435587abc73cda290793b5240a227f37a7623f8c
月華 <81902542+getuka@users.noreply.github.com>
2024-11-17 18:53:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,15 @@ setup(
     name='rubyinserter',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=requirements,
+    install_requires=[
+        "mecab-python3",
+        # ここにはPyPIからインストールできるパッケージのみを指定
+    ],
+    extras_require={
+        "dev": [
+            "git+https://github.com/miurahr/pykakasi",
+        ]
+    },
     description='A library to insert Ruby text for Japanese.',
     url='https://github.com/getuka/RubyInserter.git',
     classifiers=[
