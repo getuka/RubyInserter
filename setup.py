f8961d6191f8c6cbe9d974e876e65652b971ed95
月華 <81902542+getuka@users.noreply.github.com>
2024-11-17 18:39:40 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='rubyinserter',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=requirements,
+    description='A library to insert Ruby text for Japanese.',
+    url='https://github.com/getuka/RubyInserter.git',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
