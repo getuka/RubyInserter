6f8479c21baf91eed63824bf80348ce2598e2254
月華 <81902542+getuka@users.noreply.github.com>
2024-11-17 18:57:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
-
 setup(
     name='rubyinserter',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
         "mecab-python3",
-        # ここにはPyPIからインストールできるパッケージのみを指定
     ],
-    extras_require={
-        "dev": [
-            "git+https://github.com/miurahr/pykakasi",
-        ]
-    },
+    dependency_links=[
+        'git+https://github.com/miurahr/pykakasi#egg=pykakasi'
+    ],
     description='A library to insert Ruby text for Japanese.',
     url='https://github.com/getuka/RubyInserter.git',
     classifiers=[
